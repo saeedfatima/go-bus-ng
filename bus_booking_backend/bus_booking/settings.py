b171f7f8783f8c754544ee41ba56920c19d7cb17
saeedfatima <saidua2018@gmail.com>
2026-03-08 09:31:43 +0100

    fix(email): switch to port 465 with SSL for better reliability on render

--- bus_booking_backend/bus_booking/settings.py
+++ bus_booking_backend/bus_booking/settings.py
@@ -157,10 +157,11 @@ if os.getenv('EMAIL_HOST_USER'):
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
-EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
+EMAIL_PORT = int(os.getenv('EMAIL_PORT', 465))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'saidua2018@gmail.com')
 
 # Paystack configuration
